22e079622e2d624b29ad636e2d75252db91d505a
Ryu <215513592@qq.com>
2023-04-01 02:04:27 +0800

    1.首次

---
+++ CVSharpDemo/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.WpfExtensions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Window = System.Windows.Window;
+
+namespace CVSharpDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            new PlayVideo().Show();
+        }
+    }
+}
---
+++ CVSharpDemo/PlayVideo.xaml.cs
@@ -0,0 +1,93 @@
+﻿using HandyControl.Tools;
+using OpenCvSharp;
+using OpenCvSharp.WpfExtensions;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace CVSharpDemo
+{
+    /// <summary>
+    /// PlayVideo.xaml 的交互逻辑
+    /// </summary>
+    public partial class PlayVideo : System.Windows.Window
+    {
+        public PlayVideo()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            PlayFromFile(@"E:\电影\生化危机：诅咒.720p.BD中英双字幕.rmvb");
+        }
+
+        private void PlayFromCapture()
+        {
+            Task.Run(() =>
+            {
+                var cap = new VideoCapture(0);
+                using (Mat img = new Mat())
+                {
+                    while (true)
+                    {
+                        cap.Read(img);
+                        if (img.Empty())
+                        {
+                            break;
+                        }
+
+                        var img1 = new Mat(img.Size(), MatType.CV_8UC1);
+                        Cv2.CvtColor(img, img1, ColorConversionCodes.BGR2GRAY);
+
+                        var bitmap = img1.ToWriteableBitmap().ToBitmapImage();
+                        if (Application.Current != null)
+                        {
+                            Dispatcher.Invoke(new Action(() =>
+                            {
+                                if (image != null)
+                                {
+                                    image.Source = bitmap;
+
+                                }
+                            }));
+                        }
+                    }
+                }
+            });
+        }
+
+        private void PlayFromFile(string filename)
+        {
+            var cap = new VideoCapture(filename);
+            Task.Run(() =>
+            {
+                using (Mat img = new Mat())
+                {
+                    while (true)
+                    {
+                        cap.Read(img);
+                        if (img.Empty())
+                        {
+                            break;
+                        }
+
+                        var bitmap = img.ToWriteableBitmap().ToBitmapImage();
+                        if (Application.Current != null)
+                        {
+                            Dispatcher.Invoke(new Action(() =>
+                            {
+                                if (image != null)
+                                {
+                                    image.Source = bitmap;
+
+                                }
+                            }));
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
